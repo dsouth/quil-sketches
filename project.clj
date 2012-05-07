e515ec773e39aaa1bdf91079456ed7f26a72f298
Doug South <doug.south@gmail.com>
2012-05-07 17:54:41 +1000

    Adding dev dependencies for namespace browsing

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject quil-sketches "1.0.0-SNAPSHOT"
   :description "Sketching with quil..."
   :dependencies [[org.clojure/clojure "1.3.0"]
-		 [quil "1.4.1"]])
+                 [quil "1.4.1"]]
+  :dev-dependencies [[clj-ns-browser "1.2.0"]])
