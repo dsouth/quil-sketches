b3850ff47e8c9328dd4eebde55cb2dc301e5e419
Doug South <doug.south@gmail.com>
2012-05-07 17:07:25 +1000

    Initial stuff...

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject quil-sketches "1.0.0-SNAPSHOT"
+  :description "Sketching with quil..."
+  :dependencies [[org.clojure/clojure "1.3.0"]
+		 [quil "1.4.1"]])
---
+++ test/quil_sketches/test/core.clj
@@ -0,0 +1,6 @@
+(ns quil-sketches.test.core
+  (:use [quil-sketches.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
