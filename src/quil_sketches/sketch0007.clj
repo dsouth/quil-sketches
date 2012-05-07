cd9d529424555092970242098996303e5c1ca032
Doug South <doug.south@gmail.com>
2012-05-07 19:26:17 +1000

    Not quite what I was hoping for, but interesting all the same...

---
+++ src/quil_sketches/sketch0007.clj
@@ -0,0 +1,40 @@
+(ns quil-sketches.sketch0007
+  (:use [quil.core]))
+
+;;; Location, location, location...
+
+(defn colour-coll
+  "Returns a vector with three random numbers between 0 and 255."
+  []
+  [(random 255) (random 255) (random 255)])
+
+(defn with-random-color
+  "Takes a functions f and calls it with three random parameters
+between 0 and 255."
+  [f]
+  (apply f (colour-coll)))
+
+(defn setup []
+  (smooth)
+  (frame-rate 30)
+  (with-random-color background))
+
+(defn draw []
+  (with-random-color stroke)
+  (stroke-weight (random 10))
+  (with-random-color fill)
+
+  (let [x (random (width))
+        y (random (height))
+        mid-x (/ (width) 2)
+        mid-y (/ (height) 2)
+        radius (random 100)
+        d-width  (* radius (/ (float (abs (- mid-x x))) mid-x))
+        d-height (* radius (/ (float (abs (- mid-y y))) mid-y))]
+    (ellipse x y d-width d-height)))
+
+(defsketch example
+  :title "Oh so many colourful ellipses"
+  :setup setup
+  :draw draw
+  :size [323 200])
