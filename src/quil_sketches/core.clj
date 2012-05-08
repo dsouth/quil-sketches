59aefc440de8500beba9f82a3cb46d1c178e0b5c
Doug South <doug.south@gmail.com>
2012-05-08 19:38:08 +1000

    Moving out somewhat common functions

--- src/quil_sketches/core.clj
+++ src/quil_sketches/core.clj
@@ -1,1 +1,12 @@
 (ns quil-sketches.core)
+
+(defn colour-coll
+  "Returns a vector with three random numbers between 0 and 255."
+  []
+  [(random 255) (random 255) (random 255)])
+
+(defn with-random-color
+  "Takes a functions f and calls it with three random parameters
+between 0 and 255."
+  [f]
+  (apply f (colour-coll)))
