7ba082bbe7a0ca14f162a22113072ca3ddbb8626
Doug South <doug.south@gmail.com>
2012-05-08 19:43:45 +1000

    shades of grey with constant sized circles

---
+++ src/quil_sketches/sketch0011.clj
@@ -0,0 +1,42 @@
+(ns quil-sketches.sketch0011
+  (:use [quil.core]))
+
+;;; Lost something when modifying the size for sketch 9, so recapturing it here...
+;;; best seen when sketch is as large as possible
+
+(defn colour-coll
+  "Returns a vector with three random numbers between 0 and 255."
+  []
+  [(random 255) (random 255) (random 255)])
+
+(defn with-random-color
+  "Takes a functions f and calls it with three random parameters
+between 0 and 255."
+  [f]
+  (apply f (colour-coll)))
+
+(defn setup []
+  (smooth)
+  (frame-rate 30)
+  (stroke-weight 3))
+
+(defn percent-from-location [mid-n n]
+  (- 1 (/ (float (abs (- mid-n n))) mid-n)))
+
+(defn draw []
+  (let [x (random (width))
+        y (random (height))
+        mid-x (/ (width) 2)
+        mid-y (/ (height) 2)
+        per-x (percent-from-location mid-x x)
+        per-y (percent-from-location mid-y y)
+        combined (/ (+ per-x per-y) 2)]
+    (stroke (* 255 combined))
+    (fill (* 255 (- 1 combined)))
+    (ellipse x y 50 50)))
+
+(defsketch example
+  :title "Oh so many colourful ellipses"
+  :setup setup
+  :draw draw
+  :size [323 200])
